93cf0f8d8ea77cdb8e2b63408064e860aa8379e2
Raj <rajkamal11126@gmail.com>
2021-02-20 01:44:17 +0530

    Added: Functionality to add Extent report

--- src/main/java/com/bridgelabz/base/Base.java
+++ src/main/java/com/bridgelabz/base/Base.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.base;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,18 +10,22 @@ import org.testng.annotations.BeforeTest;
 
 public class Base {
     public static WebDriver driver;
-
+    static ExtentTest test;
+    static ExtentReports report;
     @BeforeTest
-    public void setUp() throws InterruptedException {
+    public void startTest() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.linkedin.com/home");
         Thread.sleep(2000);
+        report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html");
+        test = report.startTest("Base");
     }
 
     @AfterTest
     public void tearDown(){
-        driver.quit();
+        report.endTest(test);
+        report.flush();
     }
 }
