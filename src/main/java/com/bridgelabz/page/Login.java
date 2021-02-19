a7210aba4cb087959051ef996efc1ec3bc375089
Raj <rajkamal11126@gmail.com>
2021-02-20 01:15:45 +0530

    Refactor: Functionality to add page factory

--- src/main/java/com/bridgelabz/page/Login.java
+++ src/main/java/com/bridgelabz/page/Login.java
@@ -3,21 +3,33 @@ package com.bridgelabz.page;
 import com.bridgelabz.base.Base;
 import com.bridgelabz.util.ReadData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class Login extends Base {
 
+    @FindBy(className = "input__input")
+    WebElement userName;
+
+    @FindBy(id = "session_password")
+    WebElement Password;
+
+    public Login(WebDriver driver) {
+        this.driver = driver;
+    }
     private static final String EXCEL_PATH = "C:\\Users\\Rajkamal\\IdeaProjects\\DataDrivenFramework\\src\\test\\resources\\Data.xlsx";
 
     public void LoginToApplication() throws InterruptedException {
+
         String username = ReadData.getCellValue(EXCEL_PATH, "Data", 1, 0);
         String password = ReadData.getCellValue(EXCEL_PATH, "Data", 1, 1);
 
-        driver.findElement(By.className("input__input")).sendKeys(username);
+        userName.sendKeys(username);
         Thread.sleep(1000);
-        driver.findElement(By.id("session_password")).sendKeys(password);
+        Password.sendKeys(password);
         Thread.sleep(1000);
         driver.findElement(By.className("sign-in-form__submit-button")).click();
         Thread.sleep(2000);
-
     }
 }
--- src/test/java/com/bridgelabz/LinkedInTest.java
+++ src/test/java/com/bridgelabz/LinkedInTest.java
@@ -2,13 +2,14 @@ package com.bridgelabz;
 
 import com.bridgelabz.base.Base;
 import com.bridgelabz.page.Login;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 public class LinkedInTest extends Base {
 
     @Test
     public void LoginToApplication1() throws InterruptedException {
-        Login login = new Login();
+        Login login = PageFactory.initElements(driver,Login.class);
         login.LoginToApplication();
     }
 
